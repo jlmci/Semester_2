96c37276baf68ab59ec758980a2fc947b920cd2d
jlmci <lj9092@mci4me.at>
2025-03-20 16:28:56 +0100

    Add files via upload

---
+++ Jakob L/main.py
@@ -0,0 +1,7 @@
+from my_function import build_experiment, build_person
+
+if __name__ == "__main__":
+    my_person = build_person("Jakob", "Ladurner", "male", 25)
+    my_experiment = build_experiment("Experiment 1", "20.03.2025", "Supervisor_1" , "Jakob")
+    print(my_person)
+    print(my_experiment)
