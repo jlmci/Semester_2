3f4267835aed58de3e046091a3caa6b92e0f94aa
J4nickH <ha1220@mci4me.at>
2025-03-25 11:44:09 +0100

    Update main.py

--- Janick/main.py
+++ Janick/main.py
@@ -3,7 +3,7 @@ import datetime
 
 if __name__ == "__main__":
     
-    age = input("Enter subject age:")
+    age = float(input("Enter subject age:"))
     sex = input("Enter subject gender:")
     first_name = input("Enter subject first name:")
     last_name = input("Enter subjectlast name:")
@@ -11,9 +11,10 @@ if __name__ == "__main__":
     supervisor = input("Enter supervisor:")
     #subject = input("Enter subject:")
     date  = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
+    
     person = mf.build_person(first_name, last_name, sex, age)
     experiment = mf.build_experiment(experiment_name, date, supervisor, person)
 
     print(experiment)
+ 
     
