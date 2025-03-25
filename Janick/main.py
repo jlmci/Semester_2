92b680f67f286f39f136c57b4edbfefeb3afe534
J4nickH <ha1220@mci4me.at>
2025-03-25 11:28:19 +0100

    Update main.py
    Fehler bei verständis von Subject behoben

--- Janick/main.py
+++ Janick/main.py
@@ -3,17 +3,17 @@ import datetime
 
 if __name__ == "__main__":
     
-    #age = input("Enter age:")
-    #sex = input("Enter gender:")
-    #first_name = input("Enter first name:")
-    #last_name = input("Enter last name:")
+    age = input("Enter subject age:")
+    sex = input("Enter subject gender:")
+    first_name = input("Enter subject first name:")
+    last_name = input("Enter subjectlast name:")
     experiment_name = input("Enter experiment name:")
     supervisor = input("Enter supervisor:")
-    subject = input("Enter subject:")
+    #subject = input("Enter subject:")
     date  = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    #person = mf.build_person(first_name, last_name, sex, age)
-    experiment = mf.build_experiment(experiment_name, date, supervisor, subject)
+    person = mf.build_person(first_name, last_name, sex, age)
+    experiment = mf.build_experiment(experiment_name, date, supervisor, person)
 
     print(experiment)
     
