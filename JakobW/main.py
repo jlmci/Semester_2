00fdf4366c3a9e1c32e01f4dad1823ca6dd75a4c
WJMCI <wj2970@mci4me.at>
2025-04-03 22:14:05 +0200

    Müsterlösung eingebaut

--- JakobW/main.py
+++ JakobW/main.py
@@ -1,8 +1,32 @@
-from my_functions import build_person, build_experiment
+from my_functions import estimate_max_hr, build_person, build_experiment, ask_name, ask_number, ask_sex
+
+
 if __name__ == "__main__":
-    # build a person
-    my_dictenary= build_person("Max","Musterman", "male", 25)
-    print(my_dictenary)
-    # build an experiment
-    experiment = build_experiment("My first experiment", "20.03.2025", "Supervisor1", "Teilnehmer")
+
+    # Erstellen eines Leistungstests
+    print("Leistungstest wird erstellt")
+    print("Bitte geben Sie die Diagnostikerdaten ein:")
+
+    print(" Geben Sie den Vornamen des Diagnostikers ein:")
+    first_name = ask_name()
+    print(" Geben Sie den Nachnamen des Diagnostikers ein:")    
+    last_name = ask_name()
+
+    supervisor = build_person(first_name, last_name, None, None)
+
+    print("Bitte geben Sie die Probandendaten ein:")
+    print(" Geben Sie den Vornamen des Probanden ein:")
+    first_name = ask_name()
+    print(" Geben Sie den Nachnamen des Probanden ein:")
+    last_name = ask_name()
+    print(" Geben Sie das Alter des Probanden ein:")
+    age = ask_number()
+    sex = ask_sex()
+    # Erstellen einer Person
+    subject = build_person(first_name, last_name, sex, age)
+
+    # Erstellen eines Experiments
+    experiment = build_experiment("Leistungstest", "2021-01-01", supervisor, subject)
+
+    print("Das Experiment wurde erstellt:")
     print(experiment)
