65a11704047efd21f04dddc01c0a5603e21572e4
J4nickH <ha1220@mci4me.at>
2025-04-03 22:13:42 +0200

    changed main and my functions to Musterlösung in Janick

--- Janick/main.py
+++ Janick/main.py
@@ -1,20 +1,31 @@
-import my_functions as mf
-import datetime
+from my_functions import estimate_max_hr, build_person, build_experiment, ask_name, ask_number, ask_sex
 
 if __name__ == "__main__":
-    
-    age = float(input("Enter subject age:"))
-    sex = input("Enter subject gender:")
-    first_name = input("Enter subject first name:")
-    last_name = input("Enter subjectlast name:")
-    experiment_name = input("Enter experiment name:")
-    supervisor = input("Enter supervisor:")
-    #subject = input("Enter subject:")
-    date  = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
-    person = mf.build_person(first_name, last_name, sex, age)
-    experiment = mf.build_experiment(experiment_name, date, supervisor, person)
 
+    # Erstellen eines Leistungstests
+    print("Leistungstest wird erstellt")
+    print("Bitte geben Sie die Diagnostikerdaten ein:")
+
+    print(" Geben Sie den Vornamen des Diagnostikers ein:")
+    first_name = ask_name()
+    print(" Geben Sie den Nachnamen des Diagnostikers ein:")    
+    last_name = ask_name()
+
+    supervisor = build_person(first_name, last_name, None, None)
+
+    print("Bitte geben Sie die Probandendaten ein:")
+    print(" Geben Sie den Vornamen des Probanden ein:")
+    first_name = ask_name()
+    print(" Geben Sie den Nachnamen des Probanden ein:")
+    last_name = ask_name()
+    print(" Geben Sie das Alter des Probanden ein:")
+    age = ask_number()
+    sex = ask_sex()
+    # Erstellen einer Person
+    subject = build_person(first_name, last_name, sex, age)
+
+    # Erstellen eines Experiments
+    experiment = build_experiment("Leistungstest", "2021-01-01", supervisor, subject)
+
+    print("Das Experiment wurde erstellt:")
     print(experiment)
- 
-    
