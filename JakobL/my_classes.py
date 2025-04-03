6b764ad9ca53d6e11225d65df723a3b2b99596a1
jlmci <lj9092@mci4me.at>
2025-04-03 23:19:17 +0200

    Aufgabe61

--- JakobL/my_classes.py
+++ JakobL/my_classes.py
@@ -1,21 +1,50 @@
 class Subject():
-    def __init__(self, first_name, last, sex):
-      pass
+    def __init__(self, first_name, last, sex, age):
+      self.first_name = first_name
+      self.last_name = last
+      self.sex = sex
+      self.age = age
 
     def estimate_max_hr(self):
       """A function that estimates the maximum heart rate of a subject"""
-      pass
-
+      from my_function import estimate_max_hr
+      return estimate_max_hr(self.age, self.sex)
+    
+    def get_subject_info(self):
+      return {
+          "first_name": self.first_name,
+          "last_name": self.last_name,
+          "age": self.age,
+          "sex": self.sex,
+            "estimate_max_hr": self.estimate_max_hr()
+      }
+    
 class Supervisor():
     def __init__(self, first_name, last_name):
-      pass
+      self.first_name = first_name
+      self.last_name = last_name
+
+    def get_supervisor_info(self):
+      return {
+          "first_name": self.first_name,
+          "last_name": self.last_name
+      }
 
 class Experiment():
     def __init__(self, name, date):
-      pass
+      self.name = name
+      self.date = date
 
     def add_subject(self, subject):
       self.subject = subject
 
     def add_supervisor(self, supervisor):
-      pass
+      self.supervisor = supervisor
+
+    def get_experiment_info(self):
+      return {
+          "experiment_name": self.name,
+          "date": self.date,
+          "supervisor": self.supervisor.get_supervisor_info(),
+          "subject": self.subject.get_subject_info()
+      }
--- JakobL/test.py
+++ JakobL/test.py
@@ -1,31 +1,17 @@
-from my_functions import estimate_max_hr, build_person, build_experiment, ask_name, ask_number, ask_sex
+from my_classes import Subject, Supervisor, Experiment
 
 if __name__ == "__main__":
 
     # Erstellen eines Leistungstests
-    print("Leistungstest wird erstellt")
-    print("Bitte geben Sie die Diagnostikerdaten ein:")
+    supervisor = Supervisor("FirstName", "LastName")
+    subject = Subject("FirstName", "LastName", "female", 30)
+    subject.estimate_max_hr()
 
-    print(" Geben Sie den Vornamen des Diagnostikers ein:")
-    first_name = ask_name()
-    print(" Geben Sie den Nachnamen des Diagnostikers ein:")    
-    last_name = ask_name()
+    experiment = Experiment("Leistungstest", "2021-01-01")
+    experiment.add_subject(subject)
+    experiment.add_supervisor(supervisor)
 
-    supervisor = build_person(first_name, last_name, None, None)
-
-    print("Bitte geben Sie die Probandendaten ein:")
-    print(" Geben Sie den Vornamen des Probanden ein:")
-    first_name = ask_name()
-    print(" Geben Sie den Nachnamen des Probanden ein:")
-    last_name = ask_name()
-    print(" Geben Sie das Alter des Probanden ein:")
-    age = ask_number()
-    sex = ask_sex()
-    # Erstellen einer Person
-    subject = build_person(first_name, last_name, sex, age)
-
-    # Erstellen eines Experiments
-    experiment = build_experiment("Leistungstest", "2021-01-01", supervisor, subject)
-
-    print("Das Experiment wurde erstellt:")
     print(experiment)
+
+
+    print(experiment.get_experiment_info())
